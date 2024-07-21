0bb326031816467723e00a5f3367bd690fce9ac7
Alex42 <alexcrazy42@mail.ru>
2024-07-22 01:01:36 +0300

    make ball game

--- frontend/basic/javascript_typescript/tasks/guess_number/script.js
+++ frontend/basic/javascript_typescript/tasks/guess_number/script.js
@@ -5,12 +5,170 @@ var lastResult = document.querySelector('.lastResult');
 var lowOrHi = document.querySelector('.lowOrHi');
 
 var guessSubmit = document.querySelector('.guessSubmit');
-var guessField = document.querySelector('.guessField');
+var guessField = document.getElementById("guessField");
+guessSubmit.disabled = true;
+
+var storyNameField = document.querySelector('.story-name');
+var storyText = document.getElementById('story');
+var prevStoryText = storyText.textContent;
 
 var guessCount = 1;
 var resetButton;
 
 guessSubmit.addEventListener("click", checkGuess);
+guessField.addEventListener("input", changeGuessField);
+
+storyNameField.addEventListener("input", updateName)
+
+
+function Person(first, last, age, gender, interests) {
+    this.name = {
+      first,
+      last,
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+}
+
+Person.prototype.greeting = function () {
+    alert("Hi! I'm " + this.name.first + ".");
+};
+
+function Teacher(first, last, age, gender, interests, subject) {
+    Person.call(this, first, last, age, gender, interests);
+  
+    this.subject = subject;
+}
+
+Teacher.prototype = Object.create(Person.prototype);
+
+Teacher.prototype.greeting = function () {
+    var prefix;
+  
+    if (
+      this.gender === "male" ||
+      this.gender === "Male" ||
+      this.gender === "m" ||
+      this.gender === "M"
+    ) {
+      prefix = "Mr.";
+    } else if (
+      this.gender === "female" ||
+      this.gender === "Female" ||
+      this.gender === "f" ||
+      this.gender === "F"
+    ) {
+      prefix = "Mrs.";
+    } else {
+      prefix = "Mx.";
+    }
+  
+    alert(
+      "Hello. My name is " +
+        prefix +
+        " " +
+        this.name.last +
+        ", and I teach " +
+        this.subject +
+        ".",
+    );
+};
+
+var teacher1 = new Teacher(
+    "Dave",
+    "Griffiths",
+    31,
+    "male",
+    ["football", "cookery"],
+    "mathematics",
+);
+
+
+var header = document.querySelector("header");
+var section = document.querySelector("section");
+
+var requestURL =
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+
+var request = new XMLHttpRequest();
+
+request.open("GET", requestURL);
+
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+    console.log(request.response);
+    var superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+};
+
+
+
+
+function populateHeader(jsonObj) {
+    var header = document.querySelector("header");
+    var myH1 = document.createElement("h1");
+    myH1.textContent = jsonObj["squadName"];
+    header.appendChild(myH1);
+  
+    var myPara = document.createElement("p");
+    myPara.textContent =
+      "Hometown: " + jsonObj["homeTown"] + " // Formed: " + jsonObj["formed"];
+    header.appendChild(myPara);
+}
+  
+
+function showHeroes(jsonObj) {
+    var section = document.querySelector("section");
+    var heroes = jsonObj["members"];
+  
+    for (var i = 0; i < heroes.length; i++) {
+      var myArticle = document.createElement("article");
+      var myH2 = document.createElement("h2");
+      var myPara1 = document.createElement("p");
+      var myPara2 = document.createElement("p");
+      var myPara3 = document.createElement("p");
+      var myList = document.createElement("ul");
+  
+      myH2.textContent = heroes[i].name;
+      myPara1.textContent = "Secret identity: " + heroes[i].secretIdentity;
+      myPara2.textContent = "Age: " + heroes[i].age;
+      myPara3.textContent = "Superpowers:";
+  
+      var superPowers = heroes[i].powers;
+      for (var j = 0; j < superPowers.length; j++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = superPowers[j];
+        myList.appendChild(listItem);
+      }
+  
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+      myArticle.appendChild(myList);
+  
+      section.appendChild(myArticle);
+    }
+}
+  
+
+function updateName() {
+    var storyNameFieldText = storyNameField.value;
+    storyText.textContent = prevStoryText + `, ${storyNameFieldText}`;
+}
+
+function changeGuessField() {
+    if(guessField.value == "") {
+        guessSubmit.disabled = true;
+    } else {
+        guessSubmit.disabled = false;
+    }
+}
+
 
 function checkGuess() {
     var userGuess = guessField.value;
@@ -42,6 +200,7 @@ function checkGuess() {
 
     guessCount++;
     guessField.value = "";
+    guessSubmit.disabled = true;
     guessField.focus();
 }
 
